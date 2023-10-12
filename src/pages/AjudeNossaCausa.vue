6bdeb7c15f3189097e53741c80107bad49fdd72f
Bruno Gabriel <brunospagi@gmail.com>
2023-10-12 15:52:56 -0300

    Criado Pagina Ajude Nossa Causa

---
+++ src/pages/AjudeNossaCausa.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <div style="background-image: url(https://totosdatete.org.br/wp-content/uploads/2022/06/pexels-rk-jajoria-1189673.jpg
+);
+height: 600px;
+width: 1000px;">
+      <h2 style="text-align:center">Ajude Nossa Causa</h2>
+    </div>
+    <div style="background-color: rgb(179, 176, 174) ;">
+         <h5 style="text-align:center">Com o investimento na nossa alta demanda de pesquisas e nessas campanhas de conscientização,<br> conseguimos conversar e impactar outras pessoas e os possíveis abandonadores.<br> Essa é a nossa ferramenta: educar para acabar com o abandono.</h5>
+        </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AjudeNossaCausa'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', name: 'PaginaInicial', component: () => import('pages/IndexPage.vue') },
+      { path: 'AjudeNossaCausa', name: 'AjudeNossaCausa', component: () => import('pages/AjudeNossaCausa.vue') },
       { path: 'NovoChamado', name: 'NovoChamado', component: () => import('pages/NovoChamado.vue') },
       { path: 'Mapa', name: 'Mapa', component: () => import('pages/LMapPage.vue') },
       { path: 'Inicio', name: 'inicio', component: () => import('pages/PaginaInicial.vue') }
